e222a28ea76a0bb39aea5c50f3c711aa063a6a8f
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-12 21:42:42 +0330

    convert project to modular

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,13 @@
-class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+import { Invoice } from "./classes/Invoice.js";
 
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
-
-  format() {
-    return `${this.client} owes $${this.amount} for ${this.details}`;
-  }
-}
-
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
 
 let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-console.log(invoices);
+console.log(invOne.format());
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 const type = document.querySelector("#type")! as HTMLSelectElement;
