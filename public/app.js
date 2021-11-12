5540efcdc86b162d4de0039d76c3b99faa374cb8
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-12 16:04:35 +0330

    working with dom

---
+++ public/app.js
@@ -0,0 +1,13 @@
+"use strict";
+// const anchor = document.querySelector("a")!;
+// // if (anchor) {
+// //   console.log(anchor.href);
+// // }
+// console.log(anchor.href);
+// const form = document.querySelector('form')!;
+var form = document.querySelector('.new-item-form');
+console.log(form.children);
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
