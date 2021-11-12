4cad982d99be8e14a7b4e1284e6eabcb2ce5b14c
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-12 11:49:02 +0330

    working with objects

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,30 @@
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// array with string type
+var names = ["mamad", "ali", "ahmad"];
+// names = "hello";
+names.push("zahra");
+// names.push(3);
+// names[0] = 3;
+// array with integer type
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push("shahryar");
+// array with mixed type
+var mixed = ["ken", 4, "chun-li", 8, 9];
+mixed.push("ali");
+mixed.push(453);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: "mamad",
+    belt: "black",
+    age: 30
 };
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = "ali";
+// ninja.age = "3";
+// ninja.skills = ["dfsd", "dfsd"];
+ninja = {
+    name: "yoshi",
+    belt: "orange",
+    age: 40
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,7 @@
 // array with string type
-let names = ['mamad', 'ali', 'ahmad'];
+let names = ["mamad", "ali", "ahmad"];
+
+// names = "hello";
 
 names.push("zahra");
 // names.push(3);
@@ -12,7 +14,26 @@ numbers.push(25);
 // numbers.push("shahryar");
 
 // array with mixed type
-let mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ali');
+let mixed = ["ken", 4, "chun-li", 8, 9];
+mixed.push("ali");
 mixed.push(453);
-mixed[0]=3;
+mixed[0] = 3;
+
+// objects
+
+let ninja = {
+  name: "mamad",
+  belt: "black",
+  age: 30,
+};
+
+ninja.age= 40;
+ninja.name = "ali";
+// ninja.age = "3";
+// ninja.skills = ["dfsd", "dfsd"];
+
+ninja = {
+    name: "yoshi",
+    belt: "orange",
+    age: 40,
+}
