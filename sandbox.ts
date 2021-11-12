34152d103976734543a783f5444a986a06513494
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-11-12 11:40:44 +0330

    working with arrays in typescript

--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,18 @@
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
-}
+// array with string type
+let names = ['mamad', 'ali', 'ahmad'];
 
-console.log(circ(7.5));
+names.push("zahra");
+// names.push(3);
+// names[0] = 3;
+
+// array with integer type
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+// numbers.push("shahryar");
+
+// array with mixed type
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ali');
+mixed.push(453);
+mixed[0]=3;
