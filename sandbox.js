284257583f7393e23608725348098bb597c44e16
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-13 00:27:18 +0330

    array and object

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,24 @@
-var charachter = 'mamad';
-var age = 30;
-var isBlackeBelt = false;
-charachter = 'mt';
-var circ = function (diameter) {
-    return diameter * Math.PI;
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(3);
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('mamad');
+var mixed = ['ken', 4, 'chun-li', 8, 9, true];
+mixed.push('mamad');
+mixed.push(12);
+mixed[0] = 3;
+var ninja = {
+    _name: 'mamad',
+    belt: 'black',
+    age: 32
 };
-console.log(circ(7.5));
+ninja.age = 40;
+ninja._name = 'ryu';
+ninja.age = parseInt('30');
+// ninja.skills = ['fighting', 'sneaking'];
+ninja = {
+    _name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,33 @@
-let charachter = 'mamad';
-let age = 30;
-let isBlackeBelt = false;
+let names = ['luigi', 'mario', 'yoshi'];
 
-charachter = 'mt';
+names.push('toad');
+// names.push(3);
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+
+// numbers.push('mamad');
+
+let mixed = ['ken', 4, 'chun-li', 8, 9, true];
+
+mixed.push('mamad');
+mixed.push(12);
+mixed[0] = 3;
+
+let ninja = {
+    _name: 'mamad',
+    belt: 'black',
+    age: 32
+};
+
+ninja.age = 40;
+ninja._name = 'ryu';
+ninja.age = parseInt('30');
+// ninja.skills = ['fighting', 'sneaking'];
+
+ninja = {
+    _name: 'yoshi',
+    belt: 'orange',
+    age: 40
 }
-
-console.log(circ(7.5));
