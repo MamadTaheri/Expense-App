dc0fde601a38440821e08b531f0dda7ee6bf6a4e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-13 21:52:23 +0330

    access modifires

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 // classess
 class Invoice {
-    client: string;
-    details: string;
-    amount: number;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
 
-    constructor(c: string, d: string, a: number){
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    // constructor(c: string, d: string, a: number){
+    //     this.client = c;
+    //     this.details = d;
+    //     this.amount = a;
+    // }
+
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ){}
 
     format() {
         return `${this.client} owes ${this.amount} for \$${this.details}`
@@ -22,11 +28,9 @@ let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = 'mamad';
-invTwo.amount = 400;
-
-console.log(invoices)
-
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
