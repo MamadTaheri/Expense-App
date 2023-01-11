43a0703aa2ba974343086991e85e0ad7f52dbf3e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-01-11 12:33:51 +0330

    refresh

--- src/01-classess/01-withouth classess.ts
+++
@@ -1,3 +0,0 @@
-const message1 = { title: undefined, message: undefined, id: undefined };
-const message2 = { title: undefined, message: undefined, id: undefined };
-const message3 = { title: undefined, message: undefined, id: undefined };
